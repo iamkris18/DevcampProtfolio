7871a25db3e9c62d691e5a1c3df87e03b38f8041
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-01 12:17:32 +0530

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |b|
+    Blog.create!(
+        title:"My blog Post #{b}" ,
+        body:"Blogs are a type of regularly updated websites that provide insight into a certain topic. The word blog is a combined version of the words “web” and “log.” At their inception, blogs were simply an online diary where people could keep a log about their daily lives on the web. "
+    )
+end
+
+5.times do |s|
+    Skill.create!(
+        title:"Rails #{s}",
+        percent: 85
+    )
+end
+
+puts "5skills created"
+
+9.times do |p|
+    Portfolio.create!(
+        title: "Portfolio title #{p}",
+        subtitle: "Subtitle ",
+        body: " Body for this Portfolio",
+        main_image: " https://place-hold.it/600x400",
+        thumb_image: "https://place-hold.it/350x200 "
+    )
+end
+puts "9 portfolios created"
