4a0a8070b89ea125c7bffd1b9a1ef4136664026c
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-01 13:19:09 +0530

    portfolio_items with images

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,5 +2,22 @@ class PortfoliosController < ApplicationController
 def index
     @portfolio_items=Portfolio.all
 end
+def new
+    @portfolio_items=Portfolio.new
+end
+def create
+    @portfolio_items= Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio is live" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
 end
 
