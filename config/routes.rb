2b42c2c6eec48b4dd96ee06af6d0d9341ec335b3
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-01 16:05:18 +0530

    root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+  root to: 'pages#home'
 
-  get 'pages/about'
+ # get 'home', to: 'pages#home'
 
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
