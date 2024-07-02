b72b38ff71e10f82ceb8e50b8017a02aeffd8561
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-02 10:01:48 +0530

    friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio-show'
-  #root to: 'pages#home'
-
- # get 'home', to: 'pages#home'
 
   get 'about-me', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs  
+
 end
