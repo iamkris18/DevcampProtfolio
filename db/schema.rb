0b728ca1ae20eea58c1831fe75418e695e28e577
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-03 12:50:17 +0530

    Placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240702122351) do
+ActiveRecord::Schema.define(version: 20240703064731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20240702122351) do
     t.integer  "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "badge"
+    t.string   "text"
   end
 
   create_table "topics", force: :cascade do |t|
