2f0673512b0b6144fdd2c50b0bcda7b4e48a4856
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-07-03 11:56:26 +0530

    data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
-validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+    validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+    after_initialize :set_defaults
+    def set_defaults
+        self.main_image ||= "https://place-hold.it/600x400"
+        self.thumb_image ||= "https://place-hold.it/350x200"
+    end
+
+
+
 end
